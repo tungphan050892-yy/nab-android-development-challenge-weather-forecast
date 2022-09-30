247f5bebea568cea89b6c6963164feed8ebc3913
Tom Phan <tomphan@nvg.vn>
2022-09-30 21:20:36 +0700

    [APPS-0004] UI clean up

--- weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather.forecast/WeatherForecastModule.kt
+++
@@ -1,6 +0,0 @@
-package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast
-
-import org.koin.dsl.module
-
-val weatherForecastModule = module {
-}
---
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather/forecast/WeatherForecastModule.kt
@@ -0,0 +1,29 @@
+package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast
+
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.data.datasource.remote.network.AuthInterceptor
+import vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.data.datasource.remote.network.OpenWeatherMapApi
+
+val weatherForecastModule = module {
+
+    factory { AuthInterceptor() }
+    factory { provideOkHttpClient(get()) }
+    factory { provideForecastApi(get()) }
+    single { provideRetrofit(get()) }
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl(BuildConfig.API_URL).client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+    return OkHttpClient().newBuilder().addInterceptor(authInterceptor).build()
+}
+
+fun provideForecastApi(retrofit: Retrofit): OpenWeatherMapApi = retrofit.create(OpenWeatherMapApi::class.java)
+
+
