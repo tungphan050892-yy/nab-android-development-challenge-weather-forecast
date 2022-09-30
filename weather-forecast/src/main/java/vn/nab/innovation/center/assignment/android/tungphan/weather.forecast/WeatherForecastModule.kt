cb2d91fe6099af4c0d770631717f76f7593f7f52
Tom Phan <tomphan@nvg.vn>
2022-09-30 13:52:10 +0700

    [APPS-0001] Init base app structure
    - Init the app with ScrollingActivity.kt format from Android studio
    - Init weather-forecast module as independence feature for the app, follow clean architecture for Android
    - Base config for spotless check, gradle, libs version, etc

---
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather.forecast/WeatherForecastModule.kt
@@ -0,0 +1,6 @@
+package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast
+
+import org.koin.dsl.module
+
+val weatherForecastModule = module {
+}
---
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather.forecast/ui/WeatherForecastActivity.kt
@@ -0,0 +1,5 @@
+package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherForecastActivity : AppCompatActivity()
