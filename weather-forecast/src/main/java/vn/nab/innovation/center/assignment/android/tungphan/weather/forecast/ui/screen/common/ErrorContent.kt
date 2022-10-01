1fadf643deda2127e9d96a115e970e0171d93b45
Tom Phan <tomphan@nvg.vn>
2022-10-01 17:26:00 +0700

    [APPS-0012] Introduce common ui view with compose in weather forecast module

---
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather/forecast/ui/screen/common/ErrorContent.kt
@@ -0,0 +1,30 @@
+package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.ui.screen.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ErrorContent() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = AppTheme.colors.surface)
+    ) {
+        Icon(
+            modifier = Modifier.size(100.dp, 100.dp),
+            painter = painterResource(id = R.drawable.ic_sad), contentDescription = "Sad",
+            tint = AppTheme.colors.greyscale90
+        )
+    }
+}
---
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather/forecast/ui/screen/common/Snackbar.kt
@@ -0,0 +1,28 @@
+package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.ui.screen.common
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarData
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorSnackBar(snackBarData: SnackbarData) {
+    Snackbar(
+        modifier = Modifier.padding(AppTheme.dimensions.ONE_AND_HALF_GRID_UNIT),
+        action = {
+            TextButton(onClick = { snackBarData.performAction() }) {
+                Text(
+                    text = "${snackBarData.actionLabel?.uppercase()}",
+                    style = AppTheme.typography.button,
+                    color = AppTheme.colors.secondary
+                )
+            }
+        },
+        content = {
+            Text(text = snackBarData.message, color = AppTheme.colors.white)
+        }
+    )
+}
