30f85a5de90527a5159fb86ec4544983429e08a6
Tom Phan <tomphan@nvg.vn>
2022-10-03 14:08:50 +0700

    [APPS-0018] security purpose, temporary hide appid to access one weather map API and separate it from the code base on git repository (stored it locally)

--- weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather/forecast/data/datasource/remote/network/AuthInterceptor.kt
+++ weather-forecast/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/weather/forecast/data/datasource/remote/network/AuthInterceptor.kt
@@ -2,12 +2,13 @@ package vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.da
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.BuildConfig
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         // TODO: find another way to hide this appid (api key) cause its credential
-        val url = request.url().newBuilder().addQueryParameter("appid", "345dfc20c8ea29164e83df251b0b55a3").build()
+        val url = request.url().newBuilder().addQueryParameter("appid", BuildConfig.ONE_WEATHER_MAP_APP_ID_KEY).build()
         request = request.newBuilder().url(url).build()
         return chain.proceed(request)
     }
