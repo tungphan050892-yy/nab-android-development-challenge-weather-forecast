e4c233f9b22c13f280cef31a86af6d7ac2e3c341
Tom Phan <tomphan@nvg.vn>
2022-10-03 13:38:42 +0700

    [APPS-0018] implement firebase analytics

--- app/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/NABWeatherForecastApplication.kt
+++ app/src/main/java/vn/nab/innovation/center/assignment/android/tungphan/NABWeatherForecastApplication.kt
@@ -1,16 +1,25 @@
 package vn.nab.innovation.center.assignment.android.tungphan
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import vn.nab.innovation.center.assignment.android.tungphan.weather.forecast.weatherForecastModule
 
 class NABWeatherForecastApplication: Application() {
 
+    //TODO: normally this should managed by clean architecture as FirebaseManager
+    //but for this assignment, I decided to implement this code as fast as possible
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate() {
         super.onCreate()
         initKoin()
+        initFirebase()
     }
+
     private fun initKoin() {
         val modules = weatherForecastModule
 
@@ -18,6 +27,10 @@ class NABWeatherForecastApplication: Application() {
             androidContext(this@NABWeatherForecastApplication)
             modules(modules)
         }
+    }
+
+    private fun initFirebase(){
+        firebaseAnalytics = Firebase.analytics
     }
 
 }
